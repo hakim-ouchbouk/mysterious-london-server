dbdf695c324d58951e4cea465282d4147860f9d4
Donnie101 <donnie.darker101@gmail.com>
2021-10-11 12:42:13 +0100

    added client to server

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "prod") require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("./passport-setup");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const cors = require("cors");
+const path = require("path");
+
+// const cors = require("cors");
 const attractionsRouter = require("./routers/attractions");
 const authRouter = require("./routers/auth");
 
 const app = express();
 
-const dbURL = "mongodb://localhost:27017/arcane-london";
+// const dbURL = "mongodb://localhost:27017/arcane-london";
+// const dbURLDocker = "mongodb://host.docker.internal:27017/arcane-london";
+
+const dbURL = `mongodb+srv://db-user:${process.env.DB_PASSWORD}@cluster0.1i2oo.mongodb.net/arcane-london?retryWrites=true&w=majority`;
 const store = MongoStore.create({ mongoUrl: dbURL, touchAfter: 24 * 3600 });
 
 mongoose.connect(dbURL, { useNewUrlParser: true }, () => {
@@ -19,12 +24,12 @@ mongoose.connect(dbURL, { useNewUrlParser: true }, () => {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:5000"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "http://localhost:5000"],
+//     credentials: true,
+//   })
+// );
 
 app.use(
   session({
@@ -32,19 +37,30 @@ app.use(
     resave: false,
     saveUninitialized: true,
     store,
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 1000,
+    },
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 //AUTH
 
-app.use("/", authRouter);
+app.use("/api", authRouter);
 
 //ATTRACTIONS
 
-app.use("/", attractionsRouter);
+app.use("/api", attractionsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("APP LISTENING ON PORT: ", process.env.PORT);
